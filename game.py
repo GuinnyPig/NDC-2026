c83423c3880a4ce5e98545843e33e7f42f31a728
CodeurC <teksapo9@gmail.com>
2026-05-28 16:44:55 +0200

    Modification complète du jeu
    Refactor game structure and add zombie mechanics

--- game.py
+++ game.py
@@ -1,42 +1,147 @@
 import pyxel
 
-# Définitions des dimensions de l'écran et une "tuile" de l'écran.
+# Définitions de la configuration du jeu.
 SCREEN_SIZE = 256
 TILE_SIZE = 32
 
-CAVE = [0, 32, 0]
-CAVE2 = [0, 32, 32]
 VOID = [0, 0, 0]
-CASTEL = [0, 64, 0]
 
-# Matrice de 8x8 avec chaque case contenant un tableau de 3 nombres:
-#   numéro de la banque d'images, abscisse et ordonnée dans l'image.
-# La matrice ici représente l'écran.
+CAVE_1 = [0, 32, 0]
+CAVE_2 = [0, 32, 32]
+BAKERY = [0, 64, 0]
+
+WALL = [0, 96, 0]
+
+ZOMBIE = [0, 128, 0]
+
+PATH = [0, 96, 0]
+PATH_2 = [0, 96, 32]
+PATH_3 = [0, 96, 64]
+PATH_4 = [0, 96, 96]
+
+defeat = True
+
+# 'matrix' ici représente le terrain de jeu. C'est une matrice
+# de 8x8 avec chaque case contenant un tableau de 3 nombres:
+#### Numéro de la banque d'images, Abscisse et Ordonnée dans l'image.
 matrix = [[VOID for line in range(8)] for column in range(8)]
-matrix[0][0] = CAVE
-matrix[1][0] = CAVE2
-class App:
+
+matrix[0][0] = CAVE_1
+matrix[1][0] = CAVE_2
+matrix[6][7] = BAKERY
+
+test_person = [0, 0]
+zombies = [[0, 0]]
+idx_zombie = 0
+dir_r_or_d = True
+
+config = 0
+
+def priority(case, coef=1):
+    if case[1] < 7*coef:
+        if dir_r_or_d:
+            matrix[case[1]//coef][case[0]//coef] = PATH_2
+        else:
+            matrix[case[1]//coef][case[0]//coef] = PATH
+        case[1] += 1
+    elif case[0] < 7*coef:
+        if dir_r_or_d:
+            matrix[case[1]//coef][case[0]//coef] = PATH_4
+        else:
+            matrix[case[1]//coef][case[0]//coef] = PATH_3
+        case[0] += 1
+    return case
+
+def path_finder(case, config, coef=1):
+    global matrix
+    if config == 2 or config == 3 or config == 13 or config == 10 or config == 8:
+        if dir_r_or_d:
+            matrix[case[1]//coef][case[0]//coef] = PATH_2
+        else:
+            matrix[case[1]//coef][case[0]//coef] = PATH
+        case[1] += 1
+    elif config == 4 or config == 12 or config == 5:
+        if dir_r_or_d:
+            matrix[case[1]//coef][case[0]//coef] = PATH_4
+        else:
+            matrix[case[1]//coef][case[0]//coef] = PATH_3
+        case[0] += 1
+    elif config == 6 or config == 7 or config == 14:
+        return case
+    elif config == 15:
+        zombies.pop(idx_zombie)
+    else:
+        case = priority(case, coef)
+    return case
+
+class tower_defense:
+    cnt = 1
     def update(self):
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
 
     def draw(self):
+        global config
         pyxel.cls(11)
 
         # Dessin de la matrice.
         for y in range(len(matrix)):
             for x in range(len(matrix[0])):
-                if x == 7 and y == 7:
-                    pyxel.rect(x*32, y*32, TILE_SIZE, TILE_SIZE, 2)
-                else: 
-                    pyxel.blt(x*32, y*32, matrix[y][x][0], matrix[y][x][1], matrix[y][x][2], TILE_SIZE, TILE_SIZE)
+                pyxel.blt(x*32, y*32, matrix[y][x][0], matrix[y][x][1], matrix[y][x][2], TILE_SIZE, TILE_SIZE)
+        for zombie in zombies:
+            if zombie[0] != 7*8 or zombie[1] != 7*8:
+                if zombie[1] == 7*8:
+                    config += 4
+                elif matrix[zombie[1]//8+1][zombie[0]//8] == WALL:
+                    config += 4
+                if zombie[1] == 0:
+                    config += 1
+                elif matrix[zombie[1]//8-1][zombie[0]//8] == WALL:
+                    config += 1
+                if zombie[0] == 7*8:
+                    config += 2
+                elif matrix[zombie[1]//8][zombie[0]//8+1] == WALL:
+                    config += 2
+                if zombie[0] == 0:
+                    config += 8
+                elif matrix[zombie[1]//8][zombie[0]//8-1] == WALL:
+                    config += 8
+                zombie = path_finder(zombie, config, 8)
+            pyxel.blt(zombie[0]*4+8, zombie[1]*4+8, ZOMBIE[0], ZOMBIE[1], ZOMBIE[2], 16, 16)
+        self.cnt += 1
+        if self.cnt == 60:
+            zombies.append([0, 0])
+            self.cnt = 0
 
     def __init__(self):
-        pyxel.init(SCREEN_SIZE, SCREEN_SIZE, "Tower Defense")
+        global test_person, config
+        pyxel.init(SCREEN_SIZE, SCREEN_SIZE, "Zombies to Bakery")
 
         # Import des images fixes reliés aux endroits.
-        pyxel.images[CAVE[0]].load(CAVE[1], CAVE[2], "cave.png")
+        pyxel.images[CAVE_1[0]].load(CAVE_1[1], CAVE_1[2], "cave_1.png")
+        pyxel.images[CAVE_2[0]].load(CAVE_2[1], CAVE_2[2], "cave_2.png")
+        pyxel.images[BAKERY[0]].load(BAKERY[1], BAKERY[2], "bakery.png")
+        pyxel.images[ZOMBIE[0]].load(ZOMBIE[1], ZOMBIE[2], "zombie.png")
+        while test_person[0] != 7 or test_person[1] != 7:
+            if test_person[1] == 7:
+                config += 4
+            elif matrix[test_person[1]+1][test_person[0]] == WALL:
+                config += 4
+            if test_person[1] == 0:
+                config += 1
+            elif matrix[test_person[1]-1][test_person[0]] == WALL:
+                config += 1
+            if test_person[0] == 7:
+                config += 2
+            elif matrix[test_person[1]][test_person[0]+1] == WALL:
+                config += 2
+            if test_person[0] == 0:
+                config += 8
+            elif matrix[test_person[1]][test_person[0]-1] == WALL:
+                config += 8
+            test_person = path_finder(test_person, config)
+            config = 0
 
         pyxel.run(self.update, self.draw)
 
-App()
+tower_defense()
